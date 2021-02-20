e4e0ca9a5b31668a2e68716c5c8310f4002b39b2
tanzeel <tanzeelwahid111@gmail.com>
2021-02-20 23:53:46 +0500

    commerce js store

---
+++ src/components/products/Products.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Grid } from '@material-ui/core';
+import Product from './product/Product'
+import useStyles from "./styles";
+
+const Products = ( {products, onAddToCart} ) => {
+    const classes = useStyles();
+    return (
+        <main className={classes.content}>
+            <div className={classes.toolbar}/>
+            <Grid container justify="center" spacing={4}>
+                {products.map((prod) => (
+                    <Grid item key={prod.id}  xs={12} sm={6} md={4} lg={3}>
+                        <Product product={prod} onAddToCart={onAddToCart} />
+                    </Grid>
+                ))}
+            </Grid>
+        </main>
+    )
+}
+
+export default Products;
